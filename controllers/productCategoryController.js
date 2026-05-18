3f4907909a07e54a928d9149d773394ebe503d58
aswatji <aswat.ridwan@gmail.com>
2026-05-19 06:08:08 +0800

    uji coba logout

--- controllers/productCategoryController.js
+++ controllers/productCategoryController.js
@@ -10,7 +10,7 @@ class ProductCategoryController {
   // ✅ 1. Membuat Kategori Produk Baru → Invalidate cache
   async createCategory(req, res) {
     try {
-      const { name, description } = req.body;
+      const {name, description} = req.body;
 
       if (!name) {
         return res.status(400).json({
